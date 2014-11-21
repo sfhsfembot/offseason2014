06f66bb7f5f6bc412ea10e3ab7ad72fc708119c3
Fembot <Fembot@Web>
2014-11-20 17:52:35 -0800

    updated

--- src/org/usfirst/frc692/OffSeason2014/commands/passerPull.java
+++ src/org/usfirst/frc692/OffSeason2014/commands/passerPull.java
@@ -24,13 +24,12 @@ public class  passerPull extends Command {
     }
     // Called just before this Command runs the first time
     protected void initialize() {
-        Robot.pneumaticsForPasserPusher.passerPusherPull();
-        //passer pusher retracts when called
-        //AC 11/10/14  
+       Robot.pneumaticsForPasserPusher.passerPusherPull();
+       //passer pusher pulls when called
+       //ND 11/20/14
     }
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-       
     }
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
